b3e177526d7588a5bd525d33c57a0ff6786cce0a
huangjiehang <huangjiehang@gmail.com>
2026-01-16 15:12:56 +0800

    fix: ensure RMSNorm weight device matches input

--- nano_infer/nn/layernorm.py
+++ nano_infer/nn/layernorm.py
@@ -7,6 +7,9 @@ class RMSNorm(Module):
     def __init__(self, hidden_size, eps=1e-6):
         super().__init__()
         self.weight = Parameter(np.ones(hidden_size, dtype=np.float32))
+        self.eps = eps
 
     def forward(self, x):
+        if self.weight.device != x.device:
+            self.weight.to(x.device)
         return F.rms_norm(x, self.weight, self.eps)
